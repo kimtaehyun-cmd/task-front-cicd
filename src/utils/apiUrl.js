c37fb45fb1ae08722c6429eebc54df10f0761922
kimtaehyun-cmd <xogus_04@naver.com>
2024-09-11 10:13:05 +0900

    first commit

---
+++ src/utils/apiUrl.js
@@ -0,0 +1,14 @@
+const GET_TASKS_API_URL = 'https://tasktodo.aiprojectt.com/get_tasks';
+const POST_TASKS_API_URL = 'https://tasktodo.aiprojectt.com/post_task';
+const DELETE_TASKS_API_URL = 'https://tasktodo.aiprojectt.com/delete_task';
+const UPDATE_COMPLETED_TASKS_API_URL =
+  'https://tasktodo.aiprojectt.com/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://tasktodo.aiprojectt.com/update_task';
+
+export {
+  GET_TASKS_API_URL,
+  POST_TASKS_API_URL,
+  DELETE_TASKS_API_URL,
+  UPDATE_COMPLETED_TASKS_API_URL,
+  UPDATE_TASK_API_URL,
+};
